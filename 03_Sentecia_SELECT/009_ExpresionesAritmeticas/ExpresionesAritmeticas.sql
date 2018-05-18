d47a46c4cd85be09966964f852cfd5232293ff32
Henry.Puentes <>
2018-05-18 00:04:28 -0500

    Ajuste Documento

---
+++ 03_Sentecia_SELECT/009_ExpresionesAritmeticas/ExpresionesAritmeticas.sql
@@ -0,0 +1,29 @@
+SELECT
+
+    department_name,
+    location_id
+    
+FROM
+    departments;
+--    
+SELECT
+    last_name,
+    salary,
+    salary + 30
+FROM
+    employees;
+--    
+SELECT
+    last_name,
+    salary,
+    12 * salary + 100
+FROM
+    employees;
+--    
+SELECT
+    last_name,
+    salary,
+    12 * ( salary + 100 )
+FROM
+    employees;
+    
